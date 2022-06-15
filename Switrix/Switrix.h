ef4402f72f199635279f847814556dfac1368c08
jimmyneutront <w8250693648@protonmail.ch>
2022-06-15 11:03:49 -0400

    Initial Commit

---
+++ Switrix/Switrix.h
@@ -0,0 +1,18 @@
+//
+//  Switrix.h
+//  Switrix
+//
+//  Created by James Telzrow on 6/15/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Switrix.
+FOUNDATION_EXPORT double SwitrixVersionNumber;
+
+//! Project version string for Switrix.
+FOUNDATION_EXPORT const unsigned char SwitrixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Switrix/PublicHeader.h>
+
+

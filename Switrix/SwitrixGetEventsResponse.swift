fd2398ba692e11eec7b4eb07cb2e68857e609dcf
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 15:09:35 -0400

    Document SwitrixGetEventsResponse

--- Switrix/SwitrixGetEventsResponse.swift
+++ Switrix/SwitrixGetEventsResponse.swift
@@ -5,8 +5,20 @@
 //  Created by jimmyt on 6/16/22.
 //
 
+/**
+ The result of a successful call to the [messages](https://spec.matrix.org/v1.3/client-server-api/#get_matrixclientv3roomsroomidmessages) endpoint.
+ */
 public struct SwitrixGetEventsResponse {
+    /**
+     The value of the `"start"` field in the actual JSON response.
+     */
     public let start: String
+    /**
+     The value of the `"end"` field in the actual JSON response.
+     */
     public let end: String
+    /**
+     An array of `SwitrixClientEvent`a derived from the value of the `"chunk"` field in the actual JSON response.
+     */
     public let chunk: [SwitrixClientEvent]
 }

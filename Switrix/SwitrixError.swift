d6933757d324cf112a4e7441ee38038fc6aa4e7a
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 13:10:24 -0400

    Document SwitrixError

--- Switrix/SwitrixError.swift
+++ Switrix/SwitrixError.swift
@@ -5,11 +5,32 @@
 //  Created by jimmyt on 6/16/22.
 //
 
+/**
+ The Switrix Error `enum`. Created by Switrix code when something unexpected occurs.
+ */
 public enum SwitrixError: Error {
+    /**
+     Created when Switrix encounters a [Matrix Unknown Token](https://spec.matrix.org/v1.3/client-server-api/#common-error-codes) error.
+     
+     - Parameter message: The value of the `"error"` field in the [Matrix Standard error response](https://spec.matrix.org/v1.3/client-server-api/#standard-error-response) that gave rise to this error.
+     */
 case unknownToken(message:String)
+    /**
+     Created when Switrix encounters a [Matrix Missing Token](https://spec.matrix.org/v1.3/client-server-api/#common-error-codes) error.
+     
+     - Parameter message: The value of the `"error"` field in the [Matrix Standard error response](https://spec.matrix.org/v1.3/client-server-api/#standard-error-response) that gave rise to this error.
+     */
 case missingToken(message: String)
+    /**
+     Created when Switrix encounters a miscellaneous, rare, or otherwise unexpected non-Matrix error, or when Switrix encounters a Matrix error with an unrecognized [error code](https://spec.matrix.org/v1.3/client-server-api/#common-error-codes)
+     
+     - Parameter message: A brief message describing the context in which the error occured.
+     */
 case localUnknown(message: String)
     
+    /**
+     The [Matrix error code](https://spec.matrix.org/v1.3/client-server-api/#common-error-codes) associated with a `SwitrixError`, or `"none"` if no error code exists.
+     */
     var errorCode: String {
         switch self {
         case .unknownToken:
@@ -21,6 +42,9 @@ case localUnknown(message: String)
         }
     }
     
+    /**
+     The message associated with a `SwitrixError`.
+     */
     var errorMessage: String {
         switch self {
         case .unknownToken(let message):
@@ -32,6 +56,13 @@ case localUnknown(message: String)
         }
     }
     
+    /**
+     Creates a `SwitrixError` to represent the given `MatrixErrorResponse`.
+     
+     - Parameter errorResponse: The `MatrixErrorResponse` for which to create a `SwitrixError`
+     
+     - Returns A `SwitrixError` corresponding to `errorResponse`.
+     */
     static func createError(errorResponse: MatrixErrorResponse) -> SwitrixError {
         switch errorResponse.errcode {
         case "M_UNKNOWN_TOKEN":

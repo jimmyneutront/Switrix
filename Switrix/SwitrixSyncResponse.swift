6480cf7503f4a5ff3b316e5b224d1e7d4e9cfa4d
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 14:53:34 -0400

    Document SwitrixSyncResponse

--- Switrix/SwitrixSyncResponse.swift
+++ Switrix/SwitrixSyncResponse.swift
@@ -5,6 +5,12 @@
 //  Created by jimmyt on 6/16/22.
 //
 
+/**
+ The result of a successful call to the [sync](https://spec.matrix.org/v1.3/client-server-api/#get_matrixclientv3sync) endpoint. Note that this only contains the value of the `"next_batch"` field in the actual JSON response.
+ */
 public struct SwitrixSyncResponse: Decodable {
+    /**
+     The value of the `"next_batch"` field in the actual JSON response.
+     */
     public let nextBatchToken: String
 }

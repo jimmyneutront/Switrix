e7f6bc1d3815569b5e84134d4c55b213a4bb56a2
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 14:49:32 -0400

    Document MatrixErrorResponse

--- Switrix/MatrixErrorResponse.swift
+++ Switrix/MatrixErrorResponse.swift
@@ -5,7 +5,16 @@
 //  Created by jimmyt on 6/15/22.
 //
 
+/**
+ Represents a [Matrix Standard error response](https://spec.matrix.org/v1.3/client-server-api/#standard-error-response)
+ */
 struct MatrixErrorResponse: Decodable {
+    /**
+     The value of the `"errcode"` field in the Standard error response that this represents.
+     */
     let errcode: String
+    /**
+     The value of the `"error"` field in the Standard error response that this represents.
+     */
     let error: String
 }

f29ed2acd98ddd46575c8a459c72243a11f9c8ca
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 14:43:33 -0400

    Document SwitrixResponse

--- Switrix/SwitrixResponse.swift
+++ Switrix/SwitrixResponse.swift
@@ -5,7 +5,16 @@
 //  Created by jimmyt on 6/16/22.
 //
 
+/**
+ Represents a response from a call to a [Matrix Client-Server API](https://spec.matrix.org/v1.3/client-server-api/) endpoint.
+ */
 public enum SwitrixResponse<Response> {
+    /**
+     Indicates the request was successfull, and contains the `Response`.
+     */
     case success(Response)
+    /**
+     Indicates that the request failed, and contains the associated `Error`.
+     */
     case failure(Error)
 }

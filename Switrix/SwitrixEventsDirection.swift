1b413e12a22dd343bac95cd01e6c1165e7ac5a2c
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 19:57:49 -0400

    Document SwitrixEventsDirection

--- Switrix/SwitrixEventsDirection.swift
+++ Switrix/SwitrixEventsDirection.swift
@@ -5,9 +5,21 @@
 //  Created by jimmyt on 6/16/22.
 //
 
+/**
+ Specifies the direction in which events should be returned when calling a [Matrix Client-Server API](https://spec.matrix.org/v1.3/client-server-api/) endpoint that returns a list of Matrix events. `"forwards"` means that newest events will be at the end of the list, and `"backwards"` means that newest events will be at the beginning of the list.
+ */
 public enum SwitrixEventsDirection {
+    /**
+     Specifies that newest events should be at the end of the returned list of events.
+     */
     case forwards
+    /**
+     Specifies that newest events should be at the beginning of the returned list of events.
+     */
     case backwards
+    /**
+     The representation of the direction as a `String` that can be understood by a Matrix homeserver.
+     */
     var directionString: String {
         switch self {
         case .forwards:

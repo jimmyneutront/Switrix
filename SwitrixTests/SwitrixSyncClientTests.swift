8527727688b733f93ef3993bf976378cf0ad8a70
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 22:17:13 -0400

    Document SwitrixSyncClientTests

--- SwitrixTests/SwitrixSyncClientTests.swift
+++ SwitrixTests/SwitrixSyncClientTests.swift
@@ -9,6 +9,9 @@ import XCTest
 @testable import Switrix
 
 class SwitrixSyncClientTests: XCTestCase {
+    /**
+     Tests SwitrixSyncClient by ensuring we get a proper `SwitrixSyncResponse` from the Matrix Client-Server API [sync](https://spec.matrix.org/v1.3/client-server-api/#get_matrixclientv3sync) endpoint.
+     */
     func testSwitrixSync() {
         let syncExpectation = XCTestExpectation(description: "Get sync object from SwitrixClient")
         let client = SwitrixClient(homeserver: "https://matrix.org", token: ProcessInfo.processInfo.environment["MXKY"]!)

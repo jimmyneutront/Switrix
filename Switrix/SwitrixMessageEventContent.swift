12f8ee2b1fcbabedd9a10867cd0b083ebe3f5156
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 21:24:04 -0400

    Document SwitrixMessageEventContent

--- Switrix/SwitrixMessageEventContent.swift
+++ Switrix/SwitrixMessageEventContent.swift
@@ -5,7 +5,16 @@
 //  Created by jimmyt on 6/17/22.
 //
 
+/**
+ Represents the content of a Matrix text message event.
+ */
 public struct SwitrixMessageEventContent: SwitrixEventContent {
+    /**
+     The type of this message.
+     */
     public let messageType = "m.text"
+    /**
+     The body of this text message.
+     */
     public var body: String
 }

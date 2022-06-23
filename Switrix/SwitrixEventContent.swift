1f6d967f76b50039d8770e479d7581bd1912b4dd
jimmyneutront <w8250693648@protonmail.ch>
2022-06-22 21:00:30 -0400

    Document SwitrixEventContent

--- Switrix/SwitrixEventContent.swift
+++ Switrix/SwitrixEventContent.swift
@@ -5,6 +5,9 @@
 //  Created by jimmyt on 6/17/22.
 //
 
+/**
+ A protocol that a struct or class must adopt in order to represent the content of a [Matrix ClientEvent](https://spec.matrix.org/v1.3/client-server-api/#room-event-format)
+ */
 public protocol SwitrixEventContent {
     
 }
